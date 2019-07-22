965c0af74b0b6d25a33748b2d649c791aae9d279
Carlos Cabrera <ccabrera@beerealit.com>
2019-07-22 10:10:17 -0300

    small angular crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee'
+import { emptyScheduled } from 'rxjs/internal/observable/empty';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'small-angular-crud';
+    selectedEmployee: Employee = new Employee();
+
+    employeeArray: Employee[] = [
+        {
+            id: 1,
+            name: "QW",
+            country: "USA"
+        },
+        {
+            id: 2,
+            name: "RE",
+            country: "USA"
+        },
+        {
+            id: 3,
+            name: "YG",
+            country: "USA"
+        }
+    ];
+
+    addOrEdit() {
+        if (this.selectedEmployee.name && this.selectedEmployee.country && this.selectedEmployee.id === 0) {
+            this.selectedEmployee.id = this.employeeArray.length + 1;
+            this.employeeArray.push(this.selectedEmployee);
+        }
+        this.selectedEmployee = new Employee();
+    }
+
+    openEdit(e: Employee) {
+        this.selectedEmployee = e;
+    }
+
+    delete() {
+        if (confirm("Are you sure?")) {
+            //Search for the employee and left the array without the selected
+            this.employeeArray = this.employeeArray.filter(
+                x => x != this.selectedEmployee
+            );
+            //Clean the selected employee with a new one
+            this.selectedEmployee = new Employee();
+        }
+    }
+
 }
